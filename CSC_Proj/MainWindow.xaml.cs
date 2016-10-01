553f539831ae19545e032a8458c8a3e6fd768b41
Luke Wilkinson <lukejamesw@gmail.com>
2016-10-01 17:14:02 +0200

    Creating functionality for opening files
    Doesn’t for yet, minor error

--- CSC_Proj/MainWindow.xaml.cs
+++ CSC_Proj/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace CSC_Proj
 {
@@ -56,6 +57,33 @@ namespace CSC_Proj
         private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Open");
+
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = true;
+
+            // Call the ShowDialog method to show the dialog box.
+            bool? userClickedOK = openFileDialog1.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = openFileDialog1.File.OpenRead();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it the textbox.
+                    MainTextBox.AppendText(reader.ReadLine());
+                }
+                fileStream.Close();
+            }
+
         }
 
         #endregion
