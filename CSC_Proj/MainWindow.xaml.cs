9361fb6ee06639504039afd4f924d89a090a60cd
Luke Wilkinson <lukejamesw@gmail.com>
2016-10-01 16:53:00 +0200

    Added File&View code functionality

--- CSC_Proj/MainWindow.xaml.cs
+++ CSC_Proj/MainWindow.xaml.cs
@@ -25,6 +25,44 @@ namespace CSC_Proj
             InitializeComponent();
         }
 
+        //**Adding menu item functionality**
+
+        //To add menu item functionality go to xaml and add 
+        //<MenuItem Header="Font" HorizontalAlignment="Left" Width="140" Click="MenuItem_Click_Font"/>
+
+        //THIS PART: Click="MenuItem_Click_Font"
+
+        //With a descriptive name of what it is like 'Font'
+        //plz always stick to the same naming conventions, 
+        //this is a big project.
+
+        #region File Menu Items
+
+        private void MenuItem_Click_New(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("New");
+        }
+
+        private void MenuItem_Click_Save(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Save");
+        }
+
+        private void MenuItem_Click_SaveAs(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Save As");
+        }
+
+        private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open");
+        }
+
+        #endregion
+
+        #region View Menu Items
+        
+
         private void MenuItem_Click_Font(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Font");
@@ -39,5 +77,9 @@ namespace CSC_Proj
         {
             MessageBox.Show("Colour");
         }
+
+        #endregion
+
+        
     }
 }
