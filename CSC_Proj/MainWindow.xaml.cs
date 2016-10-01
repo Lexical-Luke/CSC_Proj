9131f64474ba7bf22541b8cf544296f39459b763
Luke Wilkinson <lukejamesw@gmail.com>
2016-10-01 14:48:14 +0200

    NowLuke

--- CSC_Proj/MainWindow.xaml.cs
+++ CSC_Proj/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,6 +34,8 @@ namespace CSC_Proj
             //another one
             //another commit
 	//ben is here
+
+        //so is luke
 =======
             //finally it works
 >>>>>>> origin/master
